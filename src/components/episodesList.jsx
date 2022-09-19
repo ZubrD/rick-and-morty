6e5575475cd691c1f32793bc1dc32e0ffdb820dc
Денис Зубрицкий <fort_zd@mail.ru>
2022-09-19 10:42:16 +0700

    Первоначальная настройка

---
+++ src/components/episodesList.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { episodes } from "../fakeStorage/episodes"; // Добивим импорт
+import Episode from "./episode"; // Импорт компонента 
+
+
+const EpisodesList = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        {episodes.map((episode) => (
+          <Episode key={episode.id} {...episode} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default EpisodesList;
